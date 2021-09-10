fcc48a853f9911a587138b5405f120f50807f624
Hatimalhazmi <60146633+Hatimalhazmi@users.noreply.github.com>
2021-09-10 15:30:45 -0400

    Update my_answers.py

--- src/mypkg/my_answers.py
+++ src/mypkg/my_answers.py
@@ -10,22 +10,22 @@ def numbers_and_strings():
     This is to review numbers and strings and basic operations.
     """
     # Write power of 1600
-    x = 1600 ** 2
+    x = 
 
     # Assign a string "Stevens" to a variable y
-    y = "Stevens"
+    y = 
 
     # Repeat variable y 7 times
-    z = y * 7
+    z = 
 
     # What is the length of z?
-    length = len(z)
+    length = 
 
     # Concatenate variable y with string " Great"
-    m = y + " is Great"
+    m = 
 
     # Replace "Great" with "Good" in variable m and assign it to a new variable n
-    n = m.replace("Great", "Good")
+    n = 
 
     return x, y, z, length, m, n
 
@@ -34,32 +34,30 @@ def lists():
     """
     This is to review basic operations with lists.
     """
-    n = "Hoboken is awesome"
+    n = 
 
     # Split variable n on a delimiter space into a list of substrings
-    p = n.split(" ")
+    p = 
 
     # Get all the items past the first of the third substring
-    r = p[2][1:]
+    r = 
 
     # Create a 3 x 3 matrix as nested list such that
     #   first row is [1, 4, 5]
     #   second row is [6, 10, 11]
     #   third row is [12, 17, 38]
-    A = [[1, 4, 5],
-         [6, 10, 11],
-         [12, 17, 38]]
+    A = 
 
     # Collect the items in the last column of matrix A using list comprehension
-    c = [row[-1] for row in A]
+    c = 
 
     # Collect only the even items of the diagonal of matrix A using list comprehension
-    d = [A[i][i] for i in range(len(A)) if A[i][i] % 2 == 0]
+    d = 
 
     # We can convert a single character to its underlying integer code (e.g., its ASCII byte value)
     # by passing it to the built-in ord function. Generate a list of these integers to represent
     # each character of the string "Stevens" using list comprehension.
-    o = [ord(c) for c in "Hoboken"]
+    o = 
 
     return p, r, c, d, o
 
@@ -70,32 +68,29 @@ def dictionaries():
     """
     # Create a dictionary that maps:
     #   fruit => "apple"
-    #   quantity => 17
+    #   quantity => 18
     #   color => "red"
-    f = {"fruit": "apple", "quantity": 4, "color": "red"}
+    f = }
 
     # Get the item in dictionary f that the key "fruit" maps to
-    a = f["fruit"]
-
+    a = 
     # Increase the quantity of f by 1
-    f["quantity"] += 1
+    
 
     # Create a nested dictionary where:
     #   name => {first_name => "Grace", last_name => "Hopper"} (a dictionary)
     #   jobs => ["scientist", "engineer"] (a list)
     #   age => 85
-    amazing_grace = {"name": {"first_name": "Grace", "last_name": "hopper"},
-                     "jobs": ["scientist", "engineer"],
-                     "age": 85}
+    amazing_grace =
 
     # Add "programmer" to the list of jobs Grace has
-    amazing_grace["jobs"].append("programmer")
+   
 
     # Get the third job Grace has that you recently added
-    p = amazing_grace["jobs"][-1]
+    p = 
 
     # Get the sorted keys of amazing_grace in alphabetically ascending order
-    k = list(amazing_grace.keys())
+    k = 
     k.sort()
 
     return a, f, p, k
